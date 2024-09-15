7371dc53a5caadda1d6c0c9a7d9829d32a243565
Raj Kumar <rajcheeyan@gmail.com>
2024-09-15 14:03:49 +0530

    Feature-SetUp

---
+++ src/test/java/Idempotence/example/Idempotence/IdempotenceApplicationTests.java
@@ -0,0 +1,13 @@
+package Idempotence.example.Idempotence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
